d1f93e21bc3c571c9b08755e136ac5dfde35f2ce
Alexander Kjäll <alexander.kjall@gmail.com>
2015-12-18 21:32:48 +0100

    GPLv3 license, changed name to Hackeriet Door

--- src/hackeriet.c
+++ src/hackeriet.c
@@ -1,3 +1,23 @@
+/**
+ * Copyright Alexander Kjäll 2015
+ *
+ * This file is part of Hackeriet Door.
+ *
+ * Hackeriet Door is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hackeriet Door is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hackeriet Door.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <pebble.h>
 
 #define KEY_STATUS 0
